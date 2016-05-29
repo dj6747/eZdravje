1149352ed15217d502832808014e4b2ae2674cc1
Domen <dj6747@student.uni-lj.si>
2016-05-29 13:37:48 +0000

    Začetek dela na generiranju

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -4,6 +4,7 @@ var queryUrl = baseUrl + '/query';
 
 var username = "ois.seminar";
 var password = "ois4fri";
+var pacientId = "";
 
 
 /**
@@ -31,12 +32,96 @@ function getSessionId() {
  * @return ehrId generiranega pacienta
  */
 function generirajPodatke(stPacienta) {
-  ehrId = "";
+   var ehrId = "";
+   
+   switch (stPacienta) {//TODO: kliči API
+       case 1:
+           ehrId = "2c7510a7-ca13-4690-b8f8-1ad0bd086674";
+           break;
+       case 2:
+           ehrId = "c2e3f047-701c-4e2d-aa35-55bebca047ce";
+           break;
+       case 3:
+           ehrId = "a6536166-1382-4cc8-9517-959f76ee0e12";
+           break;
+       
+       default:
+           ehrId = "2c7510a7-ca13-4690-b8f8-1ad0bd086674";
+           break;
+   }
+   
+   /*getEhrId(function(id) {
+       ehrId = id;
+       
+       
+   }); */
 
-  // TODO: Potrebno implementirati
-
-  return ehrId;
+   return ehrId;
 }
 
 
-// TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
+
+function generiraj() {
+    var el = "<select id='selObj' class='form-control' onchange='spremeniPacienta(this.value)'>\
+    <option value='0'>Izberi pacienta</option>\
+    </select>";
+    $("#patientSelectBtn").remove();
+    $("#selDiv").append(el);
+    for (var i = 1; i <= 3; i++) {
+      var ehrId = generirajPodatke(i);
+        generirajSelect(ehrId, function(opt) {
+            $("#selObj").append(opt);
+      });
+    }
+}
+
+function generirajSelect(ehrId, callback) {
+    osnovniPodatkiPacienta(ehrId, function(party) {
+         callback("<option value='"+ehrId+"'>"+party.firstNames+" "+party.lastNames+"</option>");
+    });
+}
+
+
+function osnovniPodatkiPacienta(ehrId, callback) {
+    sessionId = getSessionId();
+    
+    
+    $.ajax({
+		url: baseUrl + "/demographics/ehr/" + ehrId + "/party",
+		type: 'GET',
+		headers: {"Ehr-Session": sessionId},
+    	success: function (data) {
+			var party = data.party;
+			callback(party);
+		}
+	});
+    
+}
+
+
+function spremeniPacienta(val) {
+    if (val != 0) {
+        pacientId = val;
+        $("#ehrIdInput").val(pacientId);
+    } else {
+        pacientId = "";
+        alert("Izberi pacienta!");
+    }
+}
+
+
+function prijava() {
+    var ehrId = $("#ehrIdInput").val();
+    if (ehrId) {
+        pacientId = ehrId;
+        $("#startPage").hide();
+        $("#secondPage").show();
+    } else {
+        pacientId = "";
+        alert("Izberi pacienta!");
+    }
+}
+
+
+
+
